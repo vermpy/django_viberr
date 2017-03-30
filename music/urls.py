df83834d2638bc3f78bace381f31d113a4c1d3be
Paul Tzouris <tzouris@vermantia.com>
2017-03-30 13:02:20 +0300

    Added the navigation bar

--- music/urls.py
+++ music/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
 
     # /music/<album_id>/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # /music/<album_id>/favorite
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
 ]
--- music/views.py
+++ music/views.py
@@ -12,3 +12,18 @@ def index(request):
 def detail(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
+
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Album.DoesNotExist):
+        return render(request, 'music/detail.html', {
+            'album': album,
+            'error_message': 'You did not select a valid song.'
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
